ddaa84bf380689f0b2dae795d89144dee1e63f02
Erik Isidore <ericssonico@protonmail.ch>
2023-08-21 14:46:20 -0300

    API: Implement Delta Users module

---
+++ packages/api/src/delta/users/index.ts
@@ -0,0 +1,134 @@
+import http from '@utils/http'
+
+import type {
+  ClientContext,
+  ContextSetter
+} from '@typings/Client'
+
+import type { ServiceReturn } from '@typings/Delta'
+import type {
+  RevoltUser,
+  UserProfile,
+  UserStatus,
+  FieldsUser
+} from '@typings/User'
+
+// -> Types
+// --------
+
+type SetUserParams = {
+  display_name: string | null
+  avatar: string | null
+  status: UserStatus | null
+  profile: {
+    content: string | null
+    background: string | null
+  } | null
+  badges: number | null
+  flags: number | null
+  remove: FieldsUser
+}
+
+type SetUsernameParams = {
+  username: string
+  password: string
+}
+
+// -> Module definition
+// --------------------
+
+const DeltaUsers = (
+  _: ClientContext,
+  __: ContextSetter
+) => {
+  /**
+   * Retrieve a user's information
+   */
+  const getUser = async (
+    userId: string
+  ): ServiceReturn<RevoltUser> => {
+    const [ err, data ] = await http.get(`users/${userId}`)
+    return [ err, data ]
+  }
+
+  /**
+   * Retrieve information from the currently authenticated user.
+   */
+  const getSelf = async (): ServiceReturn<RevoltUser> => {
+    const [ err, data ] = await http.get('users/@me')
+    return [ err, data ]
+  }
+
+  /**
+   * Retrieve a user's flags.
+   */
+  const getUserFlags = async (
+    userId: string
+  ): ServiceReturn<{ flags: number }> => {
+    const [ err, data ] = await http.get(`users/${userId}/flags`)
+    return [ err, data ]
+  }
+
+  /**
+   * This returns a default avatar based on the given id.
+   */
+  const getDefaultAvatar = async (
+    userId: string
+  ): ServiceReturn<string> => {
+    const [ err, data ] = await http
+      .get(`users/${userId}/default_avatar`)
+
+    return [ err, data ]
+  }
+
+  /**
+   * Retrieve a user's profile data.
+   * Will fail if you do not have permission to access the other
+   * user's profile.
+   */
+  const getUserProfile = async (
+    userId: string
+  ): ServiceReturn<UserProfile> => {
+    const [ err, data ] = await http
+      .get(`users/${userId}/profile`)
+
+    return [ err, data ]
+  }
+
+  /**
+   * Edit currently authenticated user.
+   */
+  const setUser = async (
+    userId: string,
+    params: SetUserParams
+  ): ServiceReturn<RevoltUser> => {
+    const [ err, data ] = await http
+      .patch(`users/${userId}`, params)
+
+    return [ err, data ]
+  }
+
+  /**
+   * Changes the username of the currently authenticated user.
+   */
+  const setUsername = async (
+    params: SetUsernameParams
+  ): ServiceReturn<UserProfile> => {
+    const [ err, data ] = await http
+      .patch('users/@me/username', params)
+
+    return [ err, data ]
+  }
+
+  return {
+    getUser,
+    getSelf,
+    getUserFlags,
+    getDefaultAvatar,
+    getUserProfile,
+    setUser,
+    setUsername
+  }
+}
+
+export default DeltaUsers
