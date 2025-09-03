13c8a17e8d4656e635c5852fafe7250a60240dd9
Erik Isidore <ericssonico@protonmail.ch>
2025-09-02 23:27:02 -0300

    Use InfoCard within UserCard

--- packages/app/src/frontend/components/UserCard/index.tsx
+++ packages/app/src/frontend/components/UserCard/index.tsx
@@ -1,20 +1,12 @@
-import { styled } from '@stitched'
-
-import { Wrapper, If } from '@packages/components'
-
 import { type User } from '@store/shared/types'
 import { type Self } from '@store/auth'
 import { UserPresenceEnum } from '@store/chat'
 
+import InfoCard, {
+  type BackgroundColorVariant
+} from '@components/InfoCard'
 
 
-type BackgroundColorVariant
-  = 'transparent'
-  | '100' | 100
-  | '200' | 200
-  | '300' | 300
-  | '400' | 400
-  | '500' | 500
 
 export type UserCardProps = {
   user: User | Self
@@ -26,6 +18,7 @@ export type UserCardProps = {
   ) => any
 }
 
+type MouseEv = React.MouseEvent<HTMLDivElement>
 
 const UserCard = ({ user, ...props }: UserCardProps) => {
   const isOffline =
@@ -39,116 +32,21 @@ const UserCard = ({ user, ...props }: UserCardProps) => {
     ? 'Offline'
     : _statusText || presence || ''
 
+  const handleClick = (ev: MouseEv) => {
+    if (props.onClick) props.onClick(user, ev)
+  }
 
   return (
-    <Root
+    <InfoCard
       background={props.background || 'transparent'}
       hoverBg={props.hoverBg || 300}
-      onClick={props.onClick}
-    >
-      <Avatar avatar={user.avatar} />
-
-      <Wrapper column>
-        <Username>{ user.username }</Username>
-
-        <If condition={!!statusText}>
-          <Status>{ statusText }</Status>
-        </If>
-      </Wrapper>
-    </Root>
+      headline={user.displayName || user.username || ''}
+      lowerline={statusText}
+      image={user.avatar || ''}
+      onClick={handleClick}
+    />
   )
 }
-
-
-/*--------------------------------------------------------/
-/ -> Fragments                                            /
-/--------------------------------------------------------*/
-
-const Root = styled('div', {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  width: '100%',
-
-  padding: 5,
-  gap: 12,
-  borderRadius: '$baseRadius',
-
-  transition: 'background-color 200ms ease',
-  cursor: 'pointer',
-
-  variants: {
-    background: {
-      transparent: {
-        backgroundColor: 'transparent'
-      },
-
-      100: { backgroundColor: '$bg100' },
-      200: { backgroundColor: '$bg200' },
-      300: { backgroundColor: '$bg300' },
-      400: { backgroundColor: '$bg400' },
-      500: { backgroundColor: '$bg500' },
-    },
-
-    hoverBg: {
-      transparent: {
-        '&:hover': { backgroundColor: 'transparent' }
-      },
-
-      100: { '&:hover': { backgroundColor: '$bg100' } },
-      200: { '&:hover': { backgroundColor: '$bg200' } },
-      300: { '&:hover': { backgroundColor: '$bg300' } },
-      400: { '&:hover': { backgroundColor: '$bg400' } },
-      500: { '&:hover': { backgroundColor: '$bg500' } },
-    }
-  },
-
-  defaultVariants: {
-    background: 'transparent',
-    hoverBg: 300
-  }
-})
-
-const AvatarEl = styled('div', {
-  borderRadius: 11,
-  width: 40,
-  height: 40,
-
-  userSelect: 'none',
-
-  flexShrink: 0,
-  backgroundPosition: 'center',
-  backgroundSize: 'contain'
-})
-
-const Avatar = (props: { avatar: string | null }) => {
-
-  return (
-    <AvatarEl
-      style={{ backgroundImage: `url(${props.avatar})` }}
-    >
-    </AvatarEl>
-  )
-}
-
-const Status = styled('p', {
-  color: '$fg40',
-  fontWeight: '$medium',
-  userSelect: 'none',
-  fontSize: 14,
-  height: '1em'
-})
-
-
-const Username = styled('p', {
-  color: '$fg70',
-  userSelect: 'none',
-  fontWeight: '$medium',
-  fontSize: 15,
-  marginBottom: 2,
-  height: '1em'
-})
-
 
 
 export default UserCard
